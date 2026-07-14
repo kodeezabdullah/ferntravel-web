faf9725a9b5a0e9a1c5dcb9bcacc7161d0b9547c
kodeezabdullah <abdullah.gis.services@gmail.com>
2026-07-14 16:28:24 +0500

    Add Vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Anton, Alex_Brush, Inter, Poppins } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const anton = Anton({
@@ -39,7 +40,10 @@ export default function RootLayout({
       lang="en"
       className={`${anton.variable} ${alexBrush.variable} ${inter.variable} ${poppins.variable} h-full antialiased`}
     >
-      <body className="min-h-full bg-[#faf7f2]">{children}</body>
+      <body className="min-h-full bg-[#faf7f2]">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
